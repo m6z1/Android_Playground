d32fcb7db72eec4cb2a642036ad6fcb3253f9874
m6z1 <sonmyungj1zz@gmail.com>
2023-05-25 22:08:24 +0900

    initial commit

---
+++ 0525/app/src/main/java/com/m6z1/test_0525/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.m6z1.test_0525
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
